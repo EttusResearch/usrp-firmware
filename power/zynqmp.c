5e1a6ea8a0aa32abf9d5ee401dd4330b58563c8d
Moritz Fischer <moritz.fischer@ettus.com>
2019-02-21 09:50:37 -0800

    power: zynqmp: Add 'zynqmp' console command
    Add 'zynqmp' console command that will let one inspect status
    and set the bootmode pins.
    
    Signed-off-by: Moritz Fischer <moritz.fischer@ettus.com>
    Change-Id: Id25ef666d71d2a555873a33c37f69781f3442d57

--- power/zynqmp.c
+++ power/zynqmp.c
@@ -27,6 +27,9 @@
 #define CPUTS(outstr) cputs(CC_CHIPSET, outstr)
 #define CPRINTS(format, args...) cprints(CC_CHIPSET, format, ## args)
 
+/* ZynqMP bootmode */
+static uint8_t bootmode;
+
 /* Data structure for a GPIO operation for power sequencing */
 struct power_seq_op {
 	/* enum gpio_signal in 8 bits */
@@ -37,7 +40,24 @@ struct power_seq_op {
 };
 BUILD_ASSERT(GPIO_COUNT < 256);
 
-#if 0
+
+static const struct power_seq_op g3s0_seq[] = {
+	{ GPIO_PS_POR_L, 0, 65 },
+	{ GPIO_CORE_PMB_CNTL, 1, 5 },
+	{ GPIO_1V8_EN, 1, 5 },
+	{ GPIO_3V3_EN, 1, 5 },
+	{ GPIO_PS_POR_L, 1, 0 },
+	{ GPIO_0V9_EN, 1, 0 },
+	{ GPIO_DDR4N_VDDQ_EN, 1, 5 },
+	{ GPIO_DDR4S_VDDQ_EN, 1, 5 },
+	{ GPIO_3V6_EN, 1, 5 },
+	{ GPIO_2V5_EN, 1, 5 },
+	{ GPIO_DDR4S_VTT_EN, 1, 5 },
+	{ GPIO_DDR4N_VTT_EN, 1, 5 },
+	{ GPIO_3V3_CLK_EN, 1, 5 },
+	{ GPIO_PS_SRST_L, 1, 0 },
+};
+
 /**
  * Step through the power sequence table and do corresponding GPIO operations.
  *
@@ -58,7 +78,6 @@ static int power_seq_run(const struct power_seq_op *power_seq_ops, int op_count)
 	}
 	return 0;
 }
-#endif
 
 enum power_state power_handle_state(enum power_state state)
 {
@@ -127,3 +146,71 @@ static void power_button_changed(void)
 	}
 }
 DECLARE_HOOK(HOOK_POWER_BUTTON_CHANGE, power_button_changed, HOOK_PRIO_DEFAULT);
+
+
+#ifdef CONFIG_CMD_ZYNQMP
+
+struct boot_mode {
+	const char *name;
+	int val;
+};
+
+static const struct boot_mode modes_lookup[] =  { { "jtag", 0}, { "emmc", 6 } };
+
+static int str_to_bootmode(const char *boot_mode)
+{
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(modes_lookup); i++)
+		if (!strcasecmp(modes_lookup[i].name, boot_mode))
+			return modes_lookup[i].val;
+
+	/* default to jtag */
+	return 0;
+}
+
+static const char *bootmode_to_str(int bm)
+{
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(modes_lookup); i++)
+		if (modes_lookup[i].val == bm)
+			return modes_lookup[i].name;
+
+	/* default to jtag */
+	return 0;
+}
+
+static int command_zynqmp(int argc, char **argv)
+{
+	int rv = 0;
+
+	if (argc < 2)
+		return EC_ERROR_PARAM_COUNT;
+
+	if (!strcasecmp(argv[1], "status")) {
+		// gpioget() ...
+		ccprintf("ZynqMP status:\nPS_DONE:\t%d\nPS_INIT_L:\t%d\nPS_PROG_L:\t%d\nPS_ERR_OUT:\t%d\nPS_STAT:\t%d\n",
+			 gpio_get_level(GPIO_PS_DONE), gpio_get_level(GPIO_PS_INIT_L),
+			 gpio_get_level(GPIO_PS_PROG_L), gpio_get_level(GPIO_PS_ERR_OUT),
+			 gpio_get_level(GPIO_PS_ERR_STAT));
+	} else if (!strcasecmp(argv[1], "boot")) {
+		ccprintf("ZynqMP: Booting using bootmode: %s\n", bootmode_to_str(bootmode));
+		power_seq_run(&g3s0_seq[0], ARRAY_SIZE(g3s0_seq));
+	} else if (!strcasecmp(argv[1], "bootmode")) {
+		if (argc > 2) {
+			ccprintf("ZynqMP: Setting 'bootmode' to '%s'\n", argv[2]);
+			bootmode = str_to_bootmode(argv[2]);
+		} else {
+			ccprintf("ZynqMP: 'bootmode' is '%s'\n", bootmode_to_str(bootmode));
+		}
+	} else {
+		return EC_ERROR_PARAM1;
+	}
+
+	return rv;
+}
+DECLARE_CONSOLE_COMMAND(zynqmp, command_zynqmp,
+			"bootmode/status/boot idx [jtag|emmc] ",
+			"Misc commands for Xilinx ZynqMP based boards");
+#endif
