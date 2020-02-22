b36923e8970f8d55178e426ef075fa85bd5f7e2d
Michael Auchter <michael.auchter@ni.com>
2020-02-22 09:39:20 -0600

    temp_sensor: tmp112: support for multiple sensors
    This adds support for multiple sensors to the tmp112 driver. If multiple
    tmp112 sensors are present, this can be represented by adding a
    tmp112_sensors array describing the ports and addresses of the sensors
    to the the board file and defining TMP112_COUNT to be the length of this array.
    
    For compatibility, if TMP112_COUNT is not defined by the board, the
    driver falls back to the prior fixed I2C port and address.
    
    Signed-off-by: Michael Auchter <michael.auchter@ni.com>
    Change-Id: I7a28708423439cddb7c7faba7908a0ea2fdd6371

--- driver/temp_sensor/tmp112.c
+++ driver/temp_sensor/tmp112.c
@@ -16,26 +16,34 @@
 #define TMP112_SHIFT1 (16 - TMP112_RESOLUTION)
 #define TMP112_SHIFT2 (TMP112_RESOLUTION - 8)
 
-static int temp_val_local;
+#ifndef TMP112_COUNT
+#define TMP112_COUNT 1
+const struct tmp112_t tmp112_sensors[] = {
+	{ .port = I2C_PORT_THERMAL, .addr = TMP112_I2C_ADDR(0) },
+};
+#endif
 
-static int raw_read16(const int offset, int *data_ptr)
+static int temp_val_local[TMP112_COUNT];
+
+static int raw_read16(const struct tmp112_t *dev, const int offset,
+		      int *data_ptr)
 {
-	return i2c_read16(I2C_PORT_THERMAL, TMP112_I2C_ADDR_FLAGS,
-			  offset, data_ptr);
+	return i2c_read16(dev->port, dev->addr | I2C_FLAG_BIG_ENDIAN, offset,
+			  data_ptr);
 }
 
-static int raw_write16(const int offset, int data)
+static int raw_write16(const struct tmp112_t *dev, const int offset, int data)
 {
-	return i2c_write16(I2C_PORT_THERMAL, TMP112_I2C_ADDR_FLAGS,
-			   offset, data);
+	return i2c_write16(dev->port, dev->addr | I2C_FLAG_BIG_ENDIAN, offset,
+			   data);
 }
 
-static int get_temp(int *temp_ptr)
+static int get_temp(const struct tmp112_t *dev, int *temp_ptr)
 {
 	int rv;
 	int temp_raw = 0;
 
-	rv = raw_read16(TMP112_REG_TEMP, &temp_raw);
+	rv = raw_read16(dev, TMP112_REG_TEMP, &temp_raw);
 	if (rv < 0)
 		return rv;
 
@@ -54,23 +62,30 @@ static inline int tmp112_reg_to_c(int16_t reg)
 
 int tmp112_get_val(int idx, int *temp_ptr)
 {
-	*temp_ptr = temp_val_local;
+	if (idx >= TMP112_COUNT)
+		return EC_ERROR_INVAL;
+
+	*temp_ptr = temp_val_local[idx];
 	return EC_SUCCESS;
 }
 
 static void tmp112_poll(void)
 {
 	int temp_c = 0;
+	int i;
 
-	if (get_temp(&temp_c) == EC_SUCCESS)
-		temp_val_local = C_TO_K(tmp112_reg_to_c(temp_c));
+	for (i = 0; i < TMP112_COUNT; i++) {
+		if (get_temp(tmp112_sensors + i, &temp_c) == EC_SUCCESS)
+			temp_val_local[i] = C_TO_K(tmp112_reg_to_c(temp_c));
+	}
 }
 DECLARE_HOOK(HOOK_SECOND, tmp112_poll, HOOK_PRIO_TEMP_SENSOR);
 
 static void tmp112_init(void)
 {
-	int tmp;
+	int tmp, i;
 	int set_mask, clr_mask;
+	const struct tmp112_t *dev;
 
 	/* 12 bit mode */
 	set_mask = (3 << 5);
@@ -78,7 +93,10 @@ static void tmp112_init(void)
 	/* not oneshot mode */
 	clr_mask = BIT(7);
 
-	raw_read16(TMP112_REG_CONF, &tmp);
-	raw_write16(TMP112_REG_CONF, (tmp & ~clr_mask) | set_mask);
+	for (i = 0; i < TMP112_COUNT; i++) {
+		dev = tmp112_sensors + i;
+		raw_read16(dev, TMP112_REG_CONF, &tmp);
+		raw_write16(dev, TMP112_REG_CONF, (tmp & ~clr_mask) | set_mask);
+	}
 }
 DECLARE_HOOK(HOOK_INIT, tmp112_init, HOOK_PRIO_DEFAULT);
--- driver/temp_sensor/tmp112.h
+++ driver/temp_sensor/tmp112.h
@@ -8,17 +8,29 @@
 
 #include "i2c.h"
 
-#define TMP112_I2C_ADDR_FLAGS (0x48 | I2C_FLAG_BIG_ENDIAN)
-
 #define TMP112_REG_TEMP	0x00
 #define TMP112_REG_CONF	0x01
 #define TMP112_REG_HYST	0x02
 #define TMP112_REG_MAX	0x03
 
+/*
+ * addr_pin is determined by a0 pin connection:
+ * gnd = 0, v+ = 1, sda = 2, scl = 3
+ */
+#define TMP112_I2C_ADDR(addr_pin) (0x48 | (addr_pin & 0x3))
+
+struct tmp112_t {
+	int port;
+	int addr;
+};
+
+/* ports and addresses of all tmp112 sensors, must equal TMP112_COUNT */
+extern const struct tmp112_t tmp112_sensors[];
+
 /**
  * Get the last polled value of a sensor.
  *
- * @param idx		Index to read. (Ignored)
+ * @param idx		Index to read.
  *
  * @param temp_ptr	Destination for temperature in K.
  *
