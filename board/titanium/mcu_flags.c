86b5e1331452dc658e57a8446ee38af140178805
Joerg Hofrichter <joerg.hofrichter@ni.com>
2020-07-16 21:19:49 +0200

    board: titanium: add prints for bootmode and autoboot flags
    Signed-off-by: Joerg Hofrichter <joerg.hofrichter@ni.com>

--- board/titanium/mcu_flags.c
+++ board/titanium/mcu_flags.c
@@ -3,6 +3,7 @@
 #include "console.h"
 #include "usrp_eeprom.h"
 #include "eeproms.h"
+#include "zynqmp.h"
 
 static const struct usrp_eeprom_mcu_flags *mcu_flags;
 
@@ -24,18 +25,23 @@ static int mcu_flags_present(void)
 
 uint8_t mcu_flags_get_bootmode(void)
 {
+	uint8_t bootmode = zynqmp_str_to_bootmode("sd1ls");
 	if (mcu_flags_present())
-		return MCU_FLAGS_BOOTMODE(mcu_flags);
+		bootmode = MCU_FLAGS_BOOTMODE(mcu_flags);
 
-	return 0b1110; /* SD1LS by default */
+	ccprintf("bootmode flag: 0x%x (%s)\n", bootmode,
+		 zynqmp_bootmode_to_str(bootmode));
+	return bootmode;
 }
 
 uint8_t mcu_flags_get_autoboot(void)
 {
+	uint8_t autoboot = 0;
 	if (mcu_flags_present())
-		return MCU_FLAGS_AUTOBOOT(mcu_flags);
+		autoboot = MCU_FLAGS_AUTOBOOT(mcu_flags);
 
-	return 0;
+	ccprintf("autoboot flag: %u\n", autoboot);
+	return autoboot;
 }
 
 uint8_t mcu_flags_get_thermal_recovery(void)
